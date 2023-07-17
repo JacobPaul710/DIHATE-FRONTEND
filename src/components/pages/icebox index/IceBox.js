0b167ea24ab83025b0c117fdbd5623bd447444aa
JacobPaul710 <jacobpaulscott@outlook.com>
2023-07-17 13:33:35 -0700

    Sets up Icebox index skeleton page with functioning  get request and displays returned data.

--- src/components/pages/icebox index/IceBox.js
+++ src/components/pages/icebox index/IceBox.js
@@ -1,12 +1,46 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Icebox () {
+
+    const [meals, setMeals] = useState([]);
+
+    const fetchMeals = async () => {
+        try {
+            // let mealData = await fetch('http://localhost:4000/icebox');
+            let mealData = await fetch('https://jsonplaceholder.typicode.com/posts');
+            mealData = await mealData.json();
+            setMeals(mealData);
+        }
+        catch (err) {
+            let error = err.message;
+            console.log(error)
+        }
+    }
+
+
+    useEffect(() => {
+        fetchMeals();
+    }, []);
+
     return(
         <>
             <div><h1>Welcome to your Icebox, User!</h1></div>
-            <div><h1>Data displayed here</h1></div>
-            <div>
 
+            {meals.map((meal) => {
+                return (
+                    <div key={meal._id}>
+                        {/* <h3>{meal.name}</h3> */}
+                        <h3>{meal.id}</h3>
+                        {/* <h3>{meal.servings}</h3> */}
+                        <h3>{meal.title}</h3>
+                        <h3>{meal.body}</h3>
+                        {/* <h3>{meal.date}</h3> */}
+                    </div>
+                )
+            })}
+
+            <div>
                 <Link to="/editmeal"><button>Edit</button></Link>
                 <Link to="/newmeal"><button>Enter a new meal here!</button></Link>
                 <Link to="/deletemeal"><button>Delete</button></Link>
