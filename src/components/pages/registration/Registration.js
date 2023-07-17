7ee15b1879b2e8b62064a8f7eae87c1bf70d3dca
JacobPaul710 <jacobpaulscott@outlook.com>
2023-07-16 17:04:08 -0700

    Establishes fully functioning new user registration form, that configures the server to communicate with cookies.

--- src/components/pages/registration/Registration.js
+++ src/components/pages/registration/Registration.js
@@ -1,19 +1,70 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Registration () {
+
+    const Navigate = useNavigate();
+
+    let [newUser, setNewUser] = useState({
+        username: " ",
+        email: " ",
+        password: " "
+    })
+
+
+    function handleChange(e) {
+        setNewUser((previousFormState) => ({
+            ...previousFormState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    async function handleSubmit(e) {
+        try{
+            e.preventDefault();
+           const res = await fetch('http://localhost:4000/signup', {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(newUser),
+                credentials: "include"
+            })
+            // console.log(res);
+        }
+        catch(error) {
+            console.log(error)
+        }
+        finally {
+            e.target.reset();
+            Navigate('/icebox');
+        }
+    }
+
+
+
+
     return(
         <>
             <div>
                 <h1>You are creating a new account!</h1>
 
                 <div>
-                    <form>
-                        <input placeholder="Enter Email" />
+                    <form onSubmit={handleSubmit}>
+                        <label>Email: </label>
+                        <input type="email" placeholder="Enter Email" name="email" required onChange={handleChange}/>
+                        <div className="email error"></div>
                         <br />
-                        <input placeholder="Enter Username" />
+                        <label>Username: </label>
+                        <input type="text" placeholder="Enter Username" name="username" required onChange={handleChange}/>
+                        <div className="username error"></div>
                         <br />
-                        <input placeholder="Enter Password" />
+                        <label>Password: </label>
+                        <input type="password" placeholder="Enter Password" name="password" required onChange={handleChange}/>
+                        <div className="passsword error"></div>
                         <br />
+                       
                         <button>Confirm Signup</button>
                     </form>
                 </div>
